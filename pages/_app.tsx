5dfc46a538deece18196258f1d82acf1c99aa3cc
Tiradet <6231305034@lamnduan.mfu.ac.th>
2026-01-22 14:47:35 +0700

    Wrap Layout component with AnimatePresence for improved animation handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <AnimatePresence mode="wait">
     <Layout>
       <Component {...pageProps} />
-    </Layout>
+    </Layout></AnimatePresence>
   );
 }
 
